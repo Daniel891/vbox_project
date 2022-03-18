c3952972d7e15f17920a3aac998163a3c65d2347
dani <danielromero891@gmail.com>
2022-03-18 20:48:27 +0100

    Añadido el select para el sistema operativo pero funciona mal

--- crearmv.sh
+++ crearmv.sh
@@ -1,11 +1,39 @@
-#! /bin/bash
+#! /bin/bash -x
 
 
 #SCRIPT PARA CREAR UNA MV CON DIFERENTES OPCIONES PREESTABLECIDAS
 
 NOMBRE='';
+OS='';
+MEMORIA=1024;
 
 while [[ $NOMBRE == '' ]] ;
 do
 	read -p "Elige un nombre --> " NOMBRE;
 done
+
+PS3="Elige sistema operativo --> ";
+
+select item in "Debian_64" "Ubuntu_64" "Otro"
+do
+	case "$REPLAY" in
+		1)	SO="Debian_64";
+			break
+			;;
+		2)	SO="Ubuntu_64";
+			break
+			;;
+		3)	while [[ $OS == '' ]] ;
+			do
+				read -p "Elige un Sistema Operativo --> " OS;
+			done;
+			break
+			;;
+	esac
+done
+
+read -p "Memoria RAM en Mb (Por defecto serán 1024) --> " MEMORIA;
+
+echo "nombre" NOMBRE;
+echo "OS" OS;
+echo "Memoria" MEMORIA;
