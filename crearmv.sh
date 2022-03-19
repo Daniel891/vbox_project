162f082ee6f97b9b87d7b9244e97139120fb3675
dani <danielromero891@gmail.com>
2022-03-19 16:36:06 +0100

    Terminadas las opciones para crear mv

--- crearmv.sh
+++ crearmv.sh
@@ -5,23 +5,37 @@
 
 NOMBRE='';
 SO='';
-MEMORIA=1024;
+MEMORIA='';
 
 while [[ $NOMBRE == '' ]] ;
 do
 	read -p "Elige un nombre --> " NOMBRE;
 done
 
-PS3="Elige sistema operativo --> ";
+echo " ";
+echo "Elige Sistema Operativo";
+PS3="--> ";
 
 select item in "Debian_64" "Ubuntu_64" "RedHat_64" "Otro_64"
 do
-	case SO=$item;
-	break;
+	case $REPLY in 
+	1 | 2 | 3 | 4)	SO=$item;
+			break
+			;;
+	*)	echo "Opción no válida"
+		;;
+	esac
 done
 
-read -p "Memoria RAM en Mb (Por defecto serán 1024) --> " MEMORIA;
+echo " ";
 
+until [[ $MEMORIA == +([[:digit:]]) ]] && [ $MEMORIA -ge 1024 ] ;
+do
+	read -p "Memoria RAM en Mb (Mínimo 1024) --> " MEMORIA;
+done
+
+echo " ";
+	
 echo "nombre" $NOMBRE;
 echo "SO" $SO;
 echo "Memoria" $MEMORIA;
